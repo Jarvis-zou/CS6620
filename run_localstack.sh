0037ea10c8e51be156014ae914078792ce4fcec1
Jarvis-zou <429231345@qq.com>
2024-07-28 02:18:59 -0400

    localstack stuck fixed

--- run_localstack.sh
+++ run_localstack.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-echo "Starting LocalStack..."
-localstack start &
-
-# Wait for LocalStack to be ready
-echo "Waiting for LocalStack to be ready..."
-sleep 20
-
 # Check LocalStack status
 echo "Checking LocalStack status..."
 until curl -s http://localhost:4566/health | grep '"s3": "running"'; do
