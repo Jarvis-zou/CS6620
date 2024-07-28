b9c9b3893648b3a785e757391c8a3db95cef3e49
Jarvis-zou <429231345@qq.com>
2024-07-27 23:46:47 -0400

    port conflict fixed

--- run_localstack.sh
+++ run_localstack.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-
-# Start LocalStack in detached mode
-localstack start -d
-
-# Wait for LocalStack to be ready
-echo "Waiting for LocalStack to be ready..."
-sleep 10
-
 # Check LocalStack status
 echo "Checking LocalStack status..."
 curl http://localhost:4566/health
