50090bebf3dec6c39a8f8cb698809f549424283d
Jarvis-zou <429231345@qq.com>
2024-07-28 02:48:06 -0400

    localstack bug fixed

--- run_localstack.sh
+++ run_localstack.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
-# Check LocalStack status
-echo "Checking LocalStack status..."
-until curl -s http://localhost:4566/health | grep '"s3": "running"'; do
-  echo "Waiting for LocalStack S3 service to be running..."
-  sleep 5
-done
-
+localstack start &
 python ./REST/rest.py
-
-sleep 5
-
-echo "Checking REST API status..."
-until curl -s http://localhost:5000/health; do
-  echo "Waiting for REST API to be running..."
-  sleep 5
-done
-
-echo "REST API is running."
-
-tail -f /dev/null
