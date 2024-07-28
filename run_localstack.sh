0741d80470c492c963d580916a0efc82f4701c9a
Jarvis-zou <429231345@qq.com>
2024-07-28 00:08:26 -0400

    localstack stuck fixed

--- run_localstack.sh
+++ run_localstack.sh
@@ -1,5 +1,18 @@
-#!/bin/bash
+#!/bin/sh
+echo "Starting LocalStack..."
+
+# Start LocalStack in detached mode
 localstack start &
+
+# Wait for LocalStack to be ready
+echo "Waiting for LocalStack to be ready..."
 sleep 10
+
+# Check LocalStack status
+echo "Checking LocalStack status..."
+until curl -s http://localhost:4566/health | grep '"s3": "running"'; do
+  echo "Waiting for LocalStack S3 service to be running..."
+  sleep 5
+done
 
 python REST/rest.py
